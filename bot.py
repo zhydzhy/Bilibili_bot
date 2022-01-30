a415b89ce8ecf221bf7c7f47025def2fa3ac7f7c
Haotian Zhang <haotianzhang0220@gmail.com>
2022-01-29 22:36:16 -0800

    Add files via upload
    added skip function, solo function, stilling working on queueing

--- bot.py
+++ bot.py
@@ -5,11 +5,26 @@ import os
 import json
 
 
-SONG_LIST = []
-TOKEN = 'Put your token here'
+queue = []
+TOKEN = 'ODE3MjgzMDEwMzA3MTYyMTEy.YEHQHg.kterZGmx4Lwgne9OdFe1LMotEPM'
 
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='~')
 
+def get_info(url):
+    ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                        }],
+                    'outtmpl':'%(title)s.%(etx)s',
+                    'quiet':False
+                }
+        
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        return ydl.extract_info(url, download=False)
+    
 @client.event
 async def on_ready():
     print('Logged on as {}'.format(client))
@@ -19,40 +34,45 @@ async def on_ready():
 async def info(ctx):
     await ctx.send('Bilibili Bot, trying to play music off of bilibili.com.\rAuthor: CyberWh1t3zZ')
 
-#stream plays the audio from any supported link by youtube-dl
 @client.command()
 async def play(ctx, url):
     try:
         if not ctx.message.author.voice:
-            await ctx.send("You have to be in a voice channel to use play")
+            await ctx.send("得在语音频道里才能听歌")
             return
-        ydl_opts = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                    }],
-                'outtmpl':'%(title)s.%(etx)s',
-                'quiet':False
-            }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            song_info = ydl.extract_info(url, download=False)
 
-        channel = ctx.message.author.voice.channel
-        voice = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
-        voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
-        if voice_client == None:
-            await voice.connect()
-            voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        if len(queue) == 0:
+            await start_play(ctx, get_info(url))
         else:
-            await voice_client.move_to(channel)
-        voice_client.play(discord.FFmpegPCMAudio(song_info["formats"][0]["url"]))
+            queue.append(get_info(url))
+        
     except:
-        SONG_LIST.append(url)
         return
+    
 
-#connect to the voice channel where the user that sends the command
+@client.command()
+async def start_play(ctx, song_info):
+    queue[0] = song_info
+
+    channel = ctx.message.author.voice.channel
+    voice = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
+    voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice_client == None:
+        await voice.connect()
+        voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    else:
+        await voice_client.move_to(channel)
+
+    i = 0
+    while i < len(queue):
+        try:
+            voice_client.play(discord.FFmpegPCMAudio(queue[i]["formats"][0]["url"]))
+        except:
+            print("error")
+        i += 1
+
+
+@client.command()
 async def connect(ctx):
     channel = ctx.message.author.voice.channel
     voice = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
@@ -63,7 +83,7 @@ async def connect(ctx):
     else:
         await voice_client.move_to(channel)
 
-    
+
 
 #disconnect from voice channel
 @client.command()
@@ -72,9 +92,9 @@ async def leave(ctx):
     if voice != None:
         await voice.disconnect()
     else:
-        await ctx.send("not in a voice channel")
+        await ctx.send("没在语音里面 leave尼玛")
 
-#pause the audio currently playing
+
 @client.command()
 async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
@@ -83,7 +103,7 @@ async def pause(ctx):
     else:
         await ctx.send("Currently no audio is playing.")
 
-#resume if audio is paused
+
 @client.command()
 async def resume(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
@@ -92,14 +112,24 @@ async def resume(ctx):
     else:
         await ctx.send("The audio is not paused.")
 
-#stop the music, leaves voice channel
+
 @client.command()
 async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
     await voice.disconnect()
 
-
+@client.command()
+async def skip(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    await ctx.send("Song skipped")
+    if voice.is_playing():
+        voice.stop()
+        if len(SONG_LIST) != 0:
+            await play(ctx, SONG_LIST[0])
+        else:
+            return
+        
 
 
 client.run(TOKEN)
